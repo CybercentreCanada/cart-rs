a8984f056ad25356aa689df5ee5c44452965df64
Adam <adam.douglass@cyber.gc.ca>
2023-02-06 15:54:36 -0500

    check path format on windows

--- src/lib.rs
+++ src/lib.rs
@@ -792,13 +792,13 @@ mod tests {
         assert!(!cart_is_data_cart(test_string.as_ptr(), 0));
     }
 
-    #[cfg(unix)]
     #[test]
     fn null_unpack_calls() {
         // All functions exported should be "safe" to call with null values in any field that
         // take a pointer, it should never result in crashes, only error codes
         let input = tempfile::NamedTempFile::new().unwrap();
         let test_string = CString::new(input.path().to_str().unwrap()).unwrap();
+        println!("{}", input.path().to_str().unwrap());
         let mode = CString::new("rw").unwrap();
         let test_file = unsafe {fopen(test_string.as_ptr(), mode.as_ptr()) };
 
