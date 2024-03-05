5120f142741038c9393e1beb46098bb3211dc8c8
Adam Douglass <Adam.Douglass@cyber.gc.ca>
2024-03-05 20:41:37 +0000

    add some lint configuration

--- src/lib.rs
+++ src/lib.rs
@@ -75,6 +75,14 @@
 //!     Ok(())
 //! }
 //! ```
+#![warn(missing_docs, non_ascii_idents, trivial_numeric_casts,
+    unused_crate_dependencies, noop_method_call, single_use_lifetimes, trivial_casts,
+    unused_lifetimes, nonstandard_style, variant_size_differences)]
+#![deny(keyword_idents)]
+// #![warn(clippy::missing_docs_in_private_items)]
+#![allow(clippy::needless_return)]
+// #![allow(clippy::needless_return, clippy::while_let_on_iterator, clippy::collapsible_else_if)]
+
 
 use std::ffi::c_char;
 use std::ptr::{null, null_mut};
