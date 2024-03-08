7d20dec2412ba381b23dd8a42b7c55e9a487ebf0
Adam Douglass <Adam.Douglass@cyber.gc.ca>
2024-03-08 15:43:18 +0000

    update comments

--- cart_container/src/lib.rs
+++ cart_container/src/lib.rs
@@ -1,46 +1,46 @@
 //! The CaRT file format is used to store/transfer malware and it's associated metadata.
 //!
 //! It neuters the malware so it cannot be executed and encrypts it so anti-virus software
-//! cannot flag the CaRT file as malware.
+//! cannot flag the CaRT file as malware. This encryption is intentionally weak and 
+//! only intended for obfuscation.
+//! 
+//! File content is also compressed.
 //!
 //! ```rust
-//! use cart_container::{pack_stream, JsonMap, unpack_stream, error::CartError, digesters::default_digesters};
+//! use cart_container::{pack_stream, JsonMap, unpack_stream, digesters::default_digesters};
 //! use tempfile;
 //!
-//! fn main() -> Result<(), CartError> {
-//!     // A file to encode
-//!     let input_file = "./Cargo.toml";
-//!     let metadata_json: JsonMap = [("hello".to_owned(), serde_json::json!("world"))].into_iter().collect();
-//!     let carted_file = tempfile::NamedTempFile::new()?;
-//!     let output_file = tempfile::NamedTempFile::new()?;
+//! // A file to encode
+//! let input_file = "./Cargo.toml";
+//! let metadata_json: JsonMap = [("hello".to_owned(), serde_json::json!("world"))].into_iter().collect();
+//! let carted_file = tempfile::NamedTempFile::new().unwrap();
+//! let output_file = tempfile::NamedTempFile::new().unwrap();
 //!
-//!     // Encode file
-//!     pack_stream(
-//!         std::fs::File::open(input_file)?,
-//!         carted_file.as_file(),
-//!         Some(metadata_json.clone()),
-//!         None,
-//!         default_digesters(),
-//!         None
-//!     )?;
+//! // Encode file
+//! pack_stream(
+//!     std::fs::File::open(input_file).unwrap(),
+//!     carted_file.as_file(),
+//!     Some(metadata_json.clone()),
+//!     None,
+//!     default_digesters(),
+//!     None
+//! ).unwrap();
 //!
-//!     // Decode file
-//!     let (header, footer) = unpack_stream(
-//!         carted_file.reopen()?,
-//!         output_file.as_file(),
-//!         None
-//!     )?;
+//! // Decode file
+//! let (header, footer) = unpack_stream(
+//!     carted_file.reopen().unwrap(),
+//!     output_file.as_file(),
+//!     None
+//! ).unwrap();
 //!
-//!     let original_content = std::fs::read(input_file)?;
-//!     // the content should be preserved
-//!     assert_eq!(std::fs::read(output_file.path())?, original_content);
-//!     // the header should be exactly the same
-//!     assert_eq!(header.unwrap(), metadata_json);
-//!     // the footer should contain all the digests. we used the default set which includes length
-//!     assert_eq!(footer.unwrap().get("length"), Some(&serde_json::Value::from(original_content.len().to_string())));
+//! let original_content = std::fs::read(input_file).unwrap();
+//! // the content should be preserved
+//! assert_eq!(std::fs::read(output_file.path()).unwrap(), original_content);
+//! // the header should be exactly the same
+//! assert_eq!(header.unwrap(), metadata_json);
+//! // the footer should contain all the digests. we used the default set which includes length
+//! assert_eq!(footer.unwrap().get("length"), Some(&serde_json::Value::from(original_content.len().to_string())));
 //!
-//!     Ok(())
-//! }
 //! ```
 #![warn(missing_docs, non_ascii_idents, trivial_numeric_casts,
     unused_crate_dependencies, noop_method_call, single_use_lifetimes, trivial_casts,
--- shared_library/src/lib.rs
+++ shared_library/src/lib.rs
@@ -3,8 +3,6 @@
 //! It neuters the malware so it cannot be executed and encrypts it so anti-virus software
 //! cannot flag the CaRT file as malware.
 //!
-//! The functions, structs, and constants in the root of the package prefixed with `cart`
-//! are all exported to build a c library.
 //!
 //! ```c
 //! #include "cart.h"
@@ -32,49 +30,7 @@
 //! }
 //! ```
 //!
-//! An interfaces more suitable for calling from rust is in the [cart] module.
-//! Note that the crate is named 'cart_container' but the library is exported as 'cart'.
-//!
-//! ```rust
-//! use anyhow::Result;
-//! use ::cart::cart::{pack_stream, JsonMap, unpack_stream};
-//! use tempfile;
-//!
-//! fn main() -> Result<()> {
-//!     // A file to encode
-//!     let input_file = "./readme.md";
-//!     let metadata_json: JsonMap = [("hello".to_owned(), serde_json::json!("world"))].into_iter().collect();
-//!     let carted_file = tempfile::NamedTempFile::new()?;
-//!     let output_file = tempfile::NamedTempFile::new()?;
-//!
-//!     // Encode file
-//!     pack_stream(
-//!         std::fs::File::open(input_file)?,
-//!         carted_file.as_file(),
-//!         Some(metadata_json.clone()),
-//!         None,
-//!         cart::digesters::default_digesters(),
-//!         None
-//!     )?;
-//!
-//!     // Decode file
-//!     let (header, footer) = unpack_stream(
-//!         carted_file.reopen()?,
-//!         output_file.as_file(),
-//!         None
-//!     )?;
-//!
-//!     let original_content = std::fs::read(input_file)?;
-//!     // the content should be preserved
-//!     assert_eq!(std::fs::read(output_file.path())?, original_content);
-//!     // the header should be exactly the same
-//!     assert_eq!(header.unwrap(), metadata_json);
-//!     // the footer should contain all the digests. we used the default set which includes length
-//!     assert_eq!(footer.unwrap().get("length"), Some(&serde_json::Value::from(original_content.len().to_string())));
-//!
-//!     Ok(())
-//! }
-//! ```
+
 #![warn(missing_docs, non_ascii_idents, trivial_numeric_casts,
     unused_crate_dependencies, noop_method_call, single_use_lifetimes, trivial_casts,
     unused_lifetimes, nonstandard_style, variant_size_differences)]
